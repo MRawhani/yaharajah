1c6857296fa26876b7019391c883edbfdd9a31af
Rawhani <mohammedrawhani@gmail.com>
2019-11-02 20:18:15 +0300

    Adding manage pages

---
+++ yaharajah/src/components/booking/BookingManage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BookingManage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Booking manage</h1>
+            </div>
+        )
+    }
+}
---
+++ yaharajah/src/components/rentals/RentalManage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RentalManage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Rental manage</h1>
+            </div>
+        )
+    }
+}
