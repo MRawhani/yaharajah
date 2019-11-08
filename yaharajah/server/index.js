e28d8b481096302a8e5a4478bcaf19758f581a0c
Rawhani <mohammedrawhani@gmail.com>
2019-11-08 17:03:22 +0300

    Adding pre deploy settings

--- yaharajah/server/index.js
+++ yaharajah/server/index.js
@@ -2,18 +2,22 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const config = require("./config/dev");
+const config = require("./config");
 const FakeDb = require("./FakeDb");
 const rentalRoutes = require("./routes/rentals"),
   userRoutes = require("./routes/users"),
   bookingRoutes = require("./routes/bookings");
+const path = require("path");
 mongoose
   .connect(config.DB_URI, { useNewUrlParser: true })
   .then(() => {
-    const fakeDb = new FakeDb();
-    //fakeDb.seedDb();
-    console.log("sucsess");
-  }) 
+    if (process.env.NODE_ENV !== "production") {
+      const fakeDb = new FakeDb();
+      //fakeDb.seedDb();
+      console.log("sucsess");
+    }
+   
+  })
   .catch(err => {
     console.log(err);
   });
@@ -23,6 +27,13 @@ app.use(bodyParser.json());
 app.use("/api/v1/rentals", rentalRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/bookings", bookingRoutes);
+if (process.env.NODE_ENV === "production") {
+  const appPath = path.join(__dirname, "..", "build");
+  app.use(express.static(appPath));
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(appPath, "index.html"));
+  });
+}
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Running");
